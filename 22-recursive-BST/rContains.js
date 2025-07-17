ae41aaacb425b590fd737e79849a201ad9b42a96
Adriano D Machado <adriano@systax.com.br>
2025-07-17 15:15:39 -0300

    delete bst

--- 22-recursive-BST/rContains.js
+++ 22-recursive-BST/rContains.js
@@ -22,13 +22,13 @@ class BST {
 		return currentNode;
 	}
 
-	rtInsert(value) {
+	rInsert(value) {
 		if (this.root === null) {
 			this.root = new Node(value);
-			return;
+		} else {
+			this.#rInsert(value);
 		}
-
-		this.#rInsert(value);
+		return this;
 	}
 
 	insert(value) {
@@ -86,31 +86,74 @@ class BST {
 		}
 		return false;
 	}
+
+	minValue(currentNode) {
+		while (currentNode.left) {
+			currentNode = currentNode.left;
+		}
+		return currentNode.value;
+	}
+
+	#deleteNode(value, currentNode) {
+		if (currentNode === null) return null;
+
+		if (value < currentNode.value) {
+			currentNode.left = this.#deleteNode(value, currentNode.left);
+		} else if (value > currentNode.value) {
+			currentNode.right = this.#deleteNode(value, currentNode.right);
+		} else {
+			// 4 ways to solve value=currentNode.value
+			if (currentNode.left === null && currentNode.right === null) {
+				return null;
+			} else if (currentNode.left === null) {
+				currentNode = currentNode.right;
+			} else if (currentNode.right === null) {
+				currentNode = currentNode.left;
+			} else {
+				let subTreeMin = this.minValue(currentNode.right);
+				currentNode.value = subTreeMin;
+				currentNode.right = this.#deleteNode(subTreeMin, currentNode.right);
+			}
+		}
+		return currentNode;
+	}
+
+	deleteNode(value) {
+		this.root = this.#deleteNode(value, this.root);
+	}
 }
 
 let myBST = new BST();
+myBST.rInsert(2).rInsert(1).rInsert(3);
 
-myBST.rtInsert(47);
-myBST.rtInsert(21);
-myBST.rtInsert(76);
-myBST.insert(18);
-myBST.insert(27);
-myBST.insert(52);
-myBST.insert(82);
+// myBST.rInsert(47);
+// myBST.rInsert(21);
+// myBST.rInsert(76);
+// myBST.insert(18);
+// myBST.insert(27);
+// myBST.insert(52);
+// myBST.insert(82);
 
-console.log('BST Contains 27:');
-console.log(myBST.contains(27));
+// console.log('BST Contains 27:');
+// console.log(myBST.contains(27));
 
-console.log('\nBST Contains 17:');
-console.log(myBST.rContains(17));
+// console.log('\nBST Contains 17:');
+// console.log(myBST.rContains(17));
 
-/*
-    EXPECTED OUTPUT:
-    ----------------
-    BST Contains 27:
-    true
-    
-    BST Contains 17:
-    false
+// console.log('\nBST min all:');
+// console.log(myBST.minValue(myBST.root));
 
-*/
+// console.log('\nBST min right:');
+// console.log(myBST.minValue(myBST.root.right));
+
+console.log('BST before delete:');
+console.log(`Root: ${myBST.root.value}`);
+console.log(`Root->left: ${myBST.root.left.value}`);
+console.log(`Root->right: ${myBST.root.right.value}`);
+
+myBST.deleteNode(2);
+
+console.log('BST after delete:');
+console.log(`Root: ${myBST.root.value}`);
+console.log(`Root->left: ${myBST.root.left?.value ?? null}`);
+console.log(`Root->right: ${myBST.root.right?.value ?? null}`);
